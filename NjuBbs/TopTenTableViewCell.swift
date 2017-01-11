192746441f281fbdc5a491b77de77c54ad6c9a26
Tong ZHAN <zhantong1994@163.com>
2017-01-11 19:54:47 +0800

    实现加载十大

---
+++ NjuBbs/TopTenTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  TopTenTableViewCell.swift
+//  NjuBbs
+//
+//  Created by zhantong on 2017/1/11.
+//  Copyright © 2017年 PolarXiong. All rights reserved.
+//
+
+import UIKit
+
+class TopTenTableViewCell: UITableViewCell {
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var boardLabel: UILabel!
+    @IBOutlet weak var numReplyLabel: UILabel!
+}
