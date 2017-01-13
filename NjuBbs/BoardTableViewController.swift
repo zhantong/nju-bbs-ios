4d3950eb5a16d3a9f13c5b622cc1e58e6018019b
Tong ZHAN <zhantong1994@163.com>
2017-01-13 09:24:21 +0800

    增加版面文章列表向文章的转跳，修正有时获取不到文章列表的问题

--- NjuBbs/BoardTableViewController.swift
+++ NjuBbs/BoardTableViewController.swift
@@ -43,6 +43,7 @@ extension String {
     }
 }
 class BoardTableViewController: UITableViewController {
+    let baseUrl = "http://bbs.nju.edu.cn"
     var viaSegue = ""
     var cellDataList = [BoardCellData]()
     override func viewDidLoad() {
@@ -63,18 +64,38 @@ class BoardTableViewController: UITableViewController {
                 content.stringByRepairTr()
                 print(content)
                 if let doc = HTML(html: content, encoding: .utf8) {
+                    let tableHead = doc.at_xpath("//table[last()]/tr[1]")
+                    var columnIndexStatus = 0, columnIndexAuthor = 0, columnIndexTime = 0, columnIndexTitle = 0, columnIndexNumRead = 0
+                    for (index, headItem) in (tableHead?.xpath("./td").enumerated())! {
+                        if let item = headItem.text?.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines) {
+                            switch item {
+                            case "状态":
+                                columnIndexStatus = index + 1
+                            case "作者":
+                                columnIndexAuthor = index + 1
+                            case "日期":
+                                columnIndexTime = index + 1
+                            case "标题":
+                                columnIndexTitle = index + 1
+                            case "回帖/人气":
+                                columnIndexNumRead = index + 1
+                            default:
+                                ""
+                            }
+                        }
+                    }
                     for row in doc.xpath("//table[last()]/tr[position()>1]") {
-                        print(row.text)
-                        let status = row.at_xpath("./td[2]")
-                        let author = row.at_xpath("./td[3]")
+                        let status = row.at_xpath("./td[\(columnIndexStatus)]")
+                        let author = row.at_xpath("./td[\(columnIndexAuthor)]")
                         print(author?.text)
-                        let time = row.at_xpath("./td[5]")
+                        let time = row.at_xpath("./td[\(columnIndexTime)]")
                         print(time?.text)
-                        let title = row.at_xpath("./td[6]")
+                        let title = row.at_xpath("./td[\(columnIndexTitle)]")
                         print("title: " + (title?.text)!)
-                        let numRead = row.at_xpath("./td[7]")
+                        let numRead = row.at_xpath("./td[\(columnIndexNumRead)]")
                         print(numRead?.text)
                         let titleUrl = title?.at_xpath("./a/@href")
+                        print(titleUrl?.text)
                         self.cellDataList.append(BoardCellData(status: status?.text, author: author?.text, time: time?.text, title: title?.text, numRead: numRead?.text, titleUrl: titleUrl?.text))
                     }
                     self.tableView.reloadData()
@@ -116,7 +137,19 @@ class BoardTableViewController: UITableViewController {
 
         return cell
     }
-
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cellData = cellDataList[indexPath.row]
+        print(cellData.titleUrl)
+        self.performSegue(withIdentifier: "GoToArticle", sender: self)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "GoToArticle" {
+            if let destination = segue.destination as? ArticleTableViewController {
+                let indexPath = tableView.indexPathForSelectedRow
+                destination.viaSegue = baseUrl + "/" + cellDataList[(indexPath?.row)!].titleUrl
+            }
+        }
+    }
 
     /*
     // Override to support conditional editing of the table view.
