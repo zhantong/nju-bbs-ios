89b358bf9ad45abb6e4c6e9ea381d78364c7097a
Tong ZHAN <zhantong1994@163.com>
2017-01-14 20:32:13 +0800

    文章解析使用正则表达式，放弃Kanna 因为html内容不符合规范，Kanna解析会出错

--- NjuBbs/ArticleTableViewController.swift
+++ NjuBbs/ArticleTableViewController.swift
@@ -8,22 +8,11 @@
 
 import UIKit
 import Alamofire
-import Kanna
 
 struct ArticleCellData {
     let content: NSAttributedString!
     let author: String!
     let time: String!
-}
-extension Data {
-    var attributedString: NSAttributedString? {
-        do {
-            return try NSAttributedString(data: self, options: [NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType, NSCharacterEncodingDocumentAttribute: String.Encoding.utf8.rawValue], documentAttributes: nil)
-        } catch let error as NSError {
-            print(error.localizedDescription)
-        }
-        return nil
-    }
 }
 extension String {
     var utf8Data: Data? {
@@ -61,71 +50,43 @@ class ArticleTableViewController: UITableViewController {
             print(response.request!)
             print(response.response!)
             print(response.data!)
-            print(response.result.value?.attributedString)
             self.tableView.reloadData()
             if let data = response.result.value, var content = String(data: data, encoding: String.Encoding(rawValue: CFStringConvertEncodingToNSStringEncoding(CFStringEncoding(CFStringEncodings.GB_18030_2000.rawValue)))) {
                 content.stringByRemovingRegexMatches()
                 content.stringByRemovingUnsupportedColor()
-                print("***********")
-                print("content parsed: " + content)
-                print("***********")
-                //self.cellDataList.append(ArticleCellData(rawContent: content))
-                self.tableView.reloadData()
-                if let doc = HTML(html: content, encoding: .utf8) {
-                    for row in doc.xpath("//table") {
-                        let htmlString = row.toHTML
-                        let innerString = row.at_xpath(".//textarea")?.innerHTML
-                        print("^^^^^^^")
-                        print(innerString)
-                        print("^^^^^")
-                        do {
-                            let regex = try! NSRegularExpression(pattern: "发信人:\\s*(.*?),.*?发信站.*?\\((.*?)\\)(.*)--\\n※", options: NSRegularExpression.Options.dotMatchesLineSeparators)
-                            let nsString = innerString as! NSString
-                            let match = regex.firstMatch(in: innerString!, options: [], range: NSRange(location: 0, length: nsString.length))
-                            print(match)
-                            let numberOfRanges = match?.numberOfRanges
-                            guard numberOfRanges! >= 1 else {
-                                return
-                            }
-                            let author = (innerString as! NSString).substring(with: (match?.rangeAt(1))!)
-                            print("author: " + author)
-                            let dateTimeString = (innerString as! NSString).substring(with: (match?.rangeAt(2))!).trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
-                            print("dateTime:" + dateTimeString)
-                            var content = (innerString as! NSString).substring(with: (match?.rangeAt(3))!).trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
-                            //print("dateTime: "+dateTimeString)
-                            print("content: " + content)
-                            let dateFormatter = DateFormatter()
+                let tablesReg=try!NSRegularExpression(pattern: "<table.*?>(.*?)</table>", options: NSRegularExpression.Options.dotMatchesLineSeparators)
+                let contentNsString=content as NSString
+                let matches=tablesReg.matches(in: content, options: [], range: NSRange(location: 0, length: contentNsString.length))
+                for match in matches{
+                    let tableContent=contentNsString.substring(with: match.rangeAt(1))
+                    let textareaReg=try!NSRegularExpression(pattern: "<textarea.*?>(.*?)</textarea>", options: NSRegularExpression.Options.dotMatchesLineSeparators)
+                    let tableContentNsString=tableContent as NSString
+                    let match=textareaReg.firstMatch(in: tableContent, options: [], range: NSRange(location: 0, length: tableContentNsString.length))
+                    if match != nil{
+                        let textareaContent=tableContentNsString.substring(with: match!.rangeAt(1))
+                        let articleReg=try! NSRegularExpression(pattern: "发信人:\\s*(.*?),.*?发信站.*?\\((.*?)\\)(.*)--\\n※", options: NSRegularExpression.Options.dotMatchesLineSeparators)
+                        let textareaContentNsString=textareaContent as NSString
+                        let match=articleReg.firstMatch(in: textareaContent, options: [], range: NSRange(location: 0, length: textareaContentNsString.length))
+                        if match != nil{
+                            let author=textareaContentNsString.substring(with: match!.rangeAt(1)).trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+                            var time=textareaContentNsString.substring(with: match!.rangeAt(2)).trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+                            var content=textareaContentNsString.substring(with: match!.rangeAt(3)).trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+
+                            let dateFormatter=DateFormatter()
                             dateFormatter.dateFormat = "E MMM d HH:mm:ss yyyy"
                             dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-                            var time = ""
-                            if let dateTime = dateFormatter.date(from: dateTimeString) {
-                                dateFormatter.dateFormat = "yyyy-MM-dd HH:mm"
-                                time = dateFormatter.string(from: dateTime)
-                            } else {
-                                print("can't resolve time")
-                            }
-
+                            let dateTimeDate=dateFormatter.date(from: time) ?? Date()
+                            dateFormatter.dateFormat="yyyy-MM-dd HH:mm"
+                            time=dateFormatter.string(from: dateTimeDate)
 
                             content = content.replacingOccurrences(of: "\n", with: "<br/>")
-                            let attributedString = try NSAttributedString(data: content.data(using: .utf8)!, options: [NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType, NSCharacterEncodingDocumentAttribute: String.Encoding.utf8.rawValue], documentAttributes: nil)
-                            print("----------")
-                            print(content)
-                            print("*****")
-                            print(attributedString)
-                            print("--------")
-                            self.cellDataList.append(ArticleCellData(content: attributedString, author: author, time: time))
-//                            for i in 1..<numberOfRanges!{
-//                                let captured=(innerString as! NSString).substring(with: (match?.rangeAt(i))!)
-//                                print("captured: "+captured)
-//                            }
-
-                        } catch {
-                            let text = "no match"
+                            let attributedContent=try! NSAttributedString(data: content.data(using: .utf8)!, options: [NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType, NSCharacterEncodingDocumentAttribute: String.Encoding.utf8.rawValue], documentAttributes: nil)
+                            self.cellDataList.append(ArticleCellData(content: attributedContent, author: author, time: time))
                         }
                     }
-                    self.tableView.reloadData()
                 }
             }
+                    self.tableView.reloadData()
         })
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
